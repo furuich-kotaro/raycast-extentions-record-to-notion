abc9d148bb5253823530cde6ac30cfb725cf69be
furuichi-kotaro <54981611+furuich-kotaro@users.noreply.github.com>
2024-10-27 18:46:29 +0900

    だらだらもいい感じにタグづけするように修正

--- lib/notion.ts
+++ lib/notion.ts
@@ -239,7 +239,7 @@ export const setActivityCategoryFromTitle = (
       setValue("activityCategory", "運動");
     } else if (/睡眠/.test(lowerTitle)) {
       setValue("activityCategory", "睡眠");
-    } else if (/漫画|アニメ|youtube|tver|sns|tiktok/i.test(lowerTitle)) {
+    } else if (/漫画|アニメ|youtube|tver|sns|tiktok|ダラダラ|だらだら/i.test(lowerTitle)) {
       setValue("activityCategory", "娯楽(受動的)");
     } else if (/振り返り|反省|ログ/.test(lowerTitle)) {
       setValue("activityCategory", "振り返り・反省");
